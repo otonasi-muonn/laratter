3fc6d3f19b0e1edafcca08eabe4783831b994824
otonasi-muonn <nayudoutianzhong@gmail.com>
2025-09-05 19:17:28 +0900

    モデルファイルの設定・ルーティングの設定完了

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
 {
-    /** @use HasFactory<\Database\Factories\TweetFactory> */
-    use HasFactory;
+  use HasFactory;
+
+  protected $fillable = ['tweet'];
+
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
 }
