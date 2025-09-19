46e2b077526c207f0acfcfced490e13466128c51
otonasi-muonn <nayudoutianzhong@gmail.com>
2025-09-19 19:30:33 +0900

    検索機能実装完了

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,6 +21,9 @@
                     <x-nav-link :href="route('tweets.create')" :active="request()->routeIs('tweets.create')">
                         {{ __('Tweet作成') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('tweets.search')" :active="request()->routeIs('tweets.search')">
+                        {{ __('Tweet検索') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -49,7 +52,7 @@
                             @csrf
 
                             <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                                onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
@@ -82,6 +85,9 @@
             <x-responsive-nav-link :href="route('tweets.create')" :active="request()->routeIs('tweets.create')">
                 {{ __('Tweet作成') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('tweets.search')" :active="request()->routeIs('tweets.search')">
+                {{ __('Tweet検索') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
@@ -101,7 +107,7 @@
                     @csrf
 
                     <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                        onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
